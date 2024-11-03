6071b7cdea351fb60b4b3094789332a8dfb07c4f
Kare <xaxvu7@gmail.com>
2024-11-03 02:48:31 +0100

    Improved the autoclicker program.

--- autoclicker.c
+++ autoclicker.c
@@ -2,9 +2,10 @@
 
 int main() {
     const int INTERVAL = 50;
+    const char KEY = 'Z';
 
     while (1)
-        while (GetAsyncKeyState('Z') & 0x8000) {
+        while (GetAsyncKeyState(KEY) & 0x8000) {
             mouse_event(MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0);
             Sleep(50);
             mouse_event(MOUSEEVENTF_LEFTUP, 0, 0, 0, 0);
